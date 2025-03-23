fee8483399982a2d1f86da7cc05bbc77293e0167
ruViolence <78062896+ruViolence@users.noreply.github.com>
2025-03-23 19:04:25 +0800

    Bump polyglot version from 24.1.1 to 24.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,8 @@ repositories {
 dependencies {
     paperweight.paperDevBundle("1.21.1-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.6")
-    implementation("org.graalvm.polyglot:polyglot:24.1.1")
-    implementation("org.graalvm.polyglot:js-community:24.1.1")
+    implementation("org.graalvm.polyglot:polyglot:24.2.0")
+    implementation("org.graalvm.polyglot:js-community:24.2.0")
     implementation("dev.jorel:commandapi-bukkit-shade:9.5.3")
     compileOnly("org.jetbrains:annotations:24.1.0")
 }
